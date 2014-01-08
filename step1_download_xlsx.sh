0787c4031757c0d37059cee65ef9de8fe9ad9400
Sven Kirmess <sven.kirmess@kzone.ch>
2014-01-08 19:16:44 +0000

    wrong argument

--- step1_download_xlsx.sh
+++ step1_download_xlsx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -o BreedsPerPet.xlsx http://petsear.ch/excel/Breeds/BreedsPerPet.xlsx || exit 1
+wget -O BreedsPerPet.xlsx http://petsear.ch/excel/Breeds/BreedsPerPet.xlsx || exit 1
 
 echo "OK."
 exit 0
